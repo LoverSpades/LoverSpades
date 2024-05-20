39f3fda600e7f42cacff4f3c7dfa719dec53e1d3
sovnya <enp7s1@tutanota.com>
2024-05-20 15:41:40 +0200

    feat: Add libraries, config function and silly example in main

---
+++ src/conf.lua
@@ -0,0 +1,9 @@
+function lovr.conf(t)
+    -- TODO: Toggle for VR and Desktop/Anything without a VR headset.
+    -- t.modules.headset = false
+
+    t.window.title = "LoverSpades"
+    t.window.width = 1600
+    t.window.height = 900
+    t.window.resizable = true
+end
--- src/main.lua
+++ src/main.lua
@@ -1,6 +1,21 @@
+function lovr.load()
+
+end
+
+function lovr.update(dt)
+	if lovr.system.isKeyDown("w") then
+		print("Hello World!")
+	end
+end
+
 function lovr.draw(pass)
-	local angle = lovr.headset.getTime()
+	local pos = lovr.math.newVec3(0, 1.7, -1)
+
+	local scale = lovr.math.newVec3(1, 1, 1)
+	local quat = lovr.math.newQuat(0, 0, 1, 0)
+
+	local transform = lovr.math.newMat4(pos, scale, quat)
 
 	pass:setColor(0xff00ee, 255)
-	pass:cube(0, 1.7, -1, 0.5, angle)
+	pass:cube(transform, "line")
 end
